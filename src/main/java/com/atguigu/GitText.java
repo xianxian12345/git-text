cca9237829c1502b25a05f621e615bf3bb122094
线线 <@cqnu.com>
2023-03-06 13:18:10 +0800

    third commit

--- src/main/java/com/atguigu/GitText.java
+++ src/main/java/com/atguigu/GitText.java
@@ -4,6 +4,7 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("change1");
+        System.out.println("change2");
     }
 
 }
