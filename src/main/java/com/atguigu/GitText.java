cf234344d39220f9966e0f3f73265f83bb4adc02
线线 <@cqnu.com>
2023-03-06 13:17:25 +0800

    second commit

--- src/main/java/com/atguigu/GitText.java
+++ src/main/java/com/atguigu/GitText.java
@@ -3,6 +3,7 @@ package com.atguigu;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("change1");
     }
 
 }
